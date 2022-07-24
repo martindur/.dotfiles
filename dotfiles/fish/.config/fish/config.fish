781385cc074236bc6313eac3f6b0dcedac81ee22
Martin Durhuus <martindurhuus@gmail.com>
2022-07-24 14:44:45 +0200

    Update dotfiles with convention for GNU snow. Includes files for alacritty(terminal), fish(shell), qtile(wm) and x(xorg)

---
+++ dotfiles/fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+
+set fish_greeting		# Supresses fish's intro message
+set TERM "alacritty"		# Sets the terminal type
+set EDITOR "nvim"		# $EDITOR use neovim in terminal
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
