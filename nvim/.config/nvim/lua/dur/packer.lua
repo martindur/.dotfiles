16ae99d30aa6980bb18fd655745be8278d7951d0
Martin Durhuus <martindurhuus@gmail.com>
2022-09-19 19:47:57 +0200

    Adds vimwiki

--- nvim/.config/nvim/lua/dur/packer.lua
+++ nvim/.config/nvim/lua/dur/packer.lua
@@ -40,4 +40,7 @@ packer.startup(function(use)
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+
+    -- WIKI
+    use('vimwiki/vimwiki')
     end)
