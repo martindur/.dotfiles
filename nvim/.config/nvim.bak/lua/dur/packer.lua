0596b764cc26f6b4d3c24324ad8dbaa3f731a44a
Martin Durhuus <martindurhuus@gmail.com>
2023-04-13 13:42:31 +0200

    updated to astronvim

--- nvim/.config/nvim.bak/lua/dur/packer.lua
+++ nvim/.config/nvim.bak/lua/dur/packer.lua
@@ -9,6 +9,9 @@ return require("packer").startup(function(use)
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 	use("nvim-telescope/telescope-fzy-native.nvim")
+	-- use("nvim-telescope/telescope-frecency.nvim")
+	use("nvim-telescope/telescope-media-files.nvim")
+	use("nvim-telescope/telescope-file-browser.nvim")
 
 	use({
 		"numToStr/Comment.nvim",
@@ -28,6 +31,7 @@ return require("packer").startup(function(use)
 
 	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 	use("nvim-treesitter/playground")
+	use("nvim-treesitter/nvim-treesitter-textobjects")
 	--
 	use("mbbill/undotree")
 	use("theprimeagen/harpoon")
